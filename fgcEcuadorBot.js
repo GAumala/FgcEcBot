71c5f16374a8d9b0a10bf006c3bb2130d6573af8
Gabriel Aumala <gabriel@criptext.com>
2016-04-30 18:51:21 -0500

    send text responses

---
+++ fgcEcuadorBot.js
@@ -0,0 +1,64 @@
+var HABLA_CMD = "habla";
+
+var JIMMY_BOT = "jimmy";
+var GUASO_BOT = "guaso";
+var DANIEL_BOT = "daniel";
+
+var TEXT_TYPE = 0
+var AUDIO_TYPE = 1
+var PHOTO_TYPE = 2
+
+var JIMMY_PHRASES = ["maricon hijueputa, maricon",
+                     "te voy a sacar la puta",
+                     "por que no te vas a la verga?",
+                     "Alguien quiere salsearme?"]
+var GUASO_PHRASES = ["BUBU!, vales harta paloma",
+                     "Yo soy tu verdugo!",
+                     "soniaras conmigo papa, soy tu peor pesadilla.",
+                     "Que se lleve todo menos mi play pls"]
+var DANIEL_PHRASES = ["Si la ves a belen pls dile que me llame",
+                     "Quiero mandarle a belen los tornados de rashid por la papaya.",
+                     "aplasto todos los botones!!",
+                     "INFILCHOLO!!"]
+
+
+
+function createBotMessage(msgType, msgContent) {
+    var resp = {
+        type : msgType,
+        content : msgContent
+    }
+    return resp;
+}
+
+function generateRandomMsg(array) {
+    var rand = Math.floor(Math.random() * array.length);
+    return array[rand];
+
+}
+
+function getIndividualResponse(individual) {
+    var rand;
+    switch(individual) {
+        case JIMMY_BOT: {
+            return createBotMessage(0, generateRandomMsg(JIMMY_PHRASES));
+        }
+        case GUASO_BOT: {
+            return createBotMessage(0, generateRandomMsg(GUASO_PHRASES));
+        }
+        case DANIEL_BOT: {
+            return createBotMessage(0, generateRandomMsg(DANIEL_PHRASES));
+        }
+    }
+}
+
+module.exports = {
+    TEXT_MSG_TYPE : TEXT_TYPE,
+    AUDIO_MSG_TYPE : AUDIO_TYPE,
+    PHOTO_MSG_TYPE : PHOTO_TYPE,
+    processTextCommand : function(cmd, text) {
+        if(cmd == HABLA_CMD){
+            return getIndividualResponse(text.toLowerCase())
+        }
+    }
+};
--- telegramClient.js
+++ telegramClient.js
@@ -1,23 +1,90 @@
 var credentials = require("./botCredentials.js");
 var Client = require('node-rest-client').Client;
 var client = new Client();
+var myBot = require("./fgcEcuadorBot.js");
 
 var TELEGRAM_BASE_URL = "https://api.telegram.org/bot";
 var TELEGRAM_API = TELEGRAM_BASE_URL + credentials.BOT_TOKEN + '/';
+
 var GET_UPDATES = "getUpdates";
 var updateOffset = 0;
 
+var SEND_MESSAGE = "sendMessage";
+
+function processInlineQuery(inlineQuery){
+    //console.log("inline query: " + inlineQuery.query);
+}
+
+function isACommandMessage(text){
+    return text.charAt(0) == '/';
+}
+
+function sendBotMessage(chat_id, msg){
+    console.log("response created");
+    if(msg.type == myBot.TEXT_MSG_TYPE)
+        sendMessage(chat_id, msg.content);
+}
+
+/*
+ * This message object is guaranteed to hace a text attribute
+ */ 
+function processMessageText(message){
+    //console.log("message text: " + text);
+    if(isACommandMessage(message.text)){
+        var spaceIndex = message.text.indexOf(' ');
+        var reply;
+        var cmd;
+        if(spaceIndex != -1) {
+            cmd = message.text.substring(1, spaceIndex);
+            var str = message.text.substr(spaceIndex + 1);
+            reply = myBot.processTextCommand(cmd.toLowerCase(), str);
+        } else {
+            cmd = message.text.substr(1).toLowerCase();
+            reply = myBot.processTextCommand(cmd, "");
+        }
+        if(reply)
+            sendBotMessage(message.chat.id, reply);
+    }
+}
+
+function processMessage(message){
+    //console.log("message: " + message);
+    if(message.text)
+	processMessageText(message);
+}
+
+function processUpdate(update) {
+    updateOffset = update.update_id + 1;
+    if(update.inline_query)
+	processInlineQuery(update.inline_query);
+    if(update.message)
+        processMessage(update.message);
+}
+
+function sendMessage(chat_id, text){
+    var args ="?chat_id=" + chat_id + "&text=" + text;
+    client.get(TELEGRAM_API + SEND_MESSAGE + args, function( data, response) {
+        if(data.ok)
+            console.log("sent: text");
+    });
+
+}
+
 module.exports = {
     getUpdates : function (){
-	console.log("offset: " + updateOffset);
+	//console.log("offset: " + updateOffset);
 	client.get(TELEGRAM_API + GET_UPDATES + "?offset=" + updateOffset, function( data, response) {
 	    console.log(data);
 	    if(data.ok){
 	        var messages = data.result;
-		if (messages.length > 0)
-	            updateOffset = messages[messages.length - 1].update_id + 1;
+		var i;
+		for(i = 0; i < messages.length; i++){
+		    processUpdate(messages[i]);
+	    	}
 	    }
 	});
     }
 };
+
+
 
