63818175ef34883f20223a1b8da85d8a3ad1d2a0
Gabriel Aumala <gabrielfima@live.com>
2016-12-02 20:49:09 -0500

    delete express server
    Heroku is no longer used, delete the express server

--- index.js
+++ index.js
@@ -8,16 +8,3 @@ longPoll.addBot(fgcBot)
 //longPoll.addBot(tecoBot)
 
 longPoll.start()
-
-//Heroku needs to bind ports, so let's do some lame express server
-const express = require('express');
-const app = express();
-
-app.get('/', function (req, res) {
-    res.send('Hello World! fgcEcBot is Running A-Ok!!');
-});
-
-const port = process.env.PORT || 8080
-app.listen(port, function () {
-    console.log(`Example app listening on port ${port}!`);
-});
